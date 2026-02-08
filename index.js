77057db00b189038baa5fb955fd58988e74943c3
Dinsha S <144721994+dinshasivan@users.noreply.github.com>
2026-02-08 19:15:38 +0530

    Update server listening address to '0.0.0.0'

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
+    app.listen(PORT,"0.0.0.0" () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
