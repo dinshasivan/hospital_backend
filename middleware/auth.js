bcd71328c4efe5a9269f7969ab86cb3736e79854
dinshasivan <dinshasivan@gmail.com>
2026-02-09 13:02:27 +0530

    update authentication logic

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const protect = (req, res, next) => {
-  const token = req.cookies.authToken;
+  const token = req.cookies.token;
 
   if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
